631727ac8d937b010ed29da6b38d95bf018cde9c
Harnishnava <navaharnish5722@gmail.com>
2024-01-09 20:29:49 +0800

    style active links for navbar*

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,18 +1,38 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
+import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const navbar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Tasks", href: "/tasks" },
+  ];
+
   return (
-    <nav className="text-black flex space-x-6 mb-5 px-5 border-b items-center">
-      <Link href="/">Logo</Link>
+    <nav className="text-black flex space-x-6 mb-5 h-14 px-5 border-b items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
       <ul className="flex space-x-6">
-        <li>
-          {" "}
-          <Link href="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/">Tasks</Link>
-        </li>
+        {links.map((links) => (
+          <Link
+            className={classNames({
+              "text-gray-900": links.href === currentPath,
+              "text-gray-500": links.href !== currentPath,
+              "hover:text-gray-700 hover:underline transition-all duration-200":
+                true,
+            })}
+            key={links.label}
+            href={links.href}
+          >
+            {links.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
---
+++ app/tasks/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const tasksTracker = () => {
+  return <div>page</div>;
+};
+
+export default tasksTracker;
