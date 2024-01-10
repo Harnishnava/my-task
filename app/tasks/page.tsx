bd5961e95765038023258b66654059e9ece73d01
Harnishnava <navaharnish5722@gmail.com>
2024-01-10 19:17:59 +0800

    set up radix ui

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const tasksTracker = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <Button>New tasks</Button>
+    </div>
+  );
 };
 
 export default tasksTracker;
