96b42fded0bf4a31ba51ec3e6654be62bd80bd30
Harnishnava <navaharnish5722@gmail.com>
2024-01-10 20:43:20 +0800

    handle form submission

--- app/tasks/new-task/page.tsx
+++ app/tasks/new-task/page.tsx
@@ -1,22 +1,47 @@
 "use client";
 import React from "react";
 import { TextField, Button } from "@radix-ui/themes";
+import { useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { CiSearch } from "react-icons/ci";
+import axios from "axios";
+import { data } from "autoprefixer";
+import { useRouter } from "next/navigation";
+
+interface TaskForm {
+  title: string;
+  description: string;
+}
 
 const newTaskpage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<TaskForm>();
+
   return (
-    <div className="space-y-3 max-w-xl">
+    <form
+      className="space-y-3 max-w-xl"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/tasks", data);
+        router.push("/tasks");
+      })}
+    >
       <TextField.Root>
         <TextField.Slot>
           <CiSearch height="16" width="16" />
         </TextField.Slot>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
       <Button>Add Task</Button>
-    </div>
+    </form>
   );
 };
 
